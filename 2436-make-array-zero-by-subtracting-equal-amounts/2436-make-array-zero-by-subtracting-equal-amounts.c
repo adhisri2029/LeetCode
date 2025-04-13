ed24ac3d33d613908627a6956b4f2d708af15150
adhisri2029 <144305281+adhisri2029@users.noreply.github.com>
2025-04-13 14:57:43 +0530

    Time: 0 ms (100%), Space: 8 MB (25.26%) - LeetHub

--- 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.c
+++ 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.c
@@ -1,13 +1,17 @@
 int minimumOperations(int* nums, int numsSize) 
 {
-    int freq[101]={0};
+    int freq[101];
     int count=0;
+    for (int i=0;i<101;i++) 
+    {
+        freq[i]=0;
+    }
     for (int i=0;i<numsSize;i++) 
     {
         int num=nums[i];
         if (num>0&&freq[num]==0) 
         {
-            freq[num] = 1;
+            freq[num]=1;
             count++;
         }
     }
